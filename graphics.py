a340447b289eb9f13a77ea027be8dfc6f30a8a54
MisterGij <mistera2u@gmail.com>
2025-11-02 18:27:55 -0500

    Create Menu Bar

--- graphics.py
+++ graphics.py
@@ -1,11 +1,32 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtGui import QAction, QKeySequence, QPainter
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenuBar
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Set title
+        self.setWindowTitle("Transformations")
+
+        # Create Menu
+        menu = self.menuBar()
+        file_menu = menu.addMenu("File")
+        menu.addMenu(file_menu)
+
+        # Define and add exit action on non-MacOS systems
+        if sys.platform != "darwin":
+            exit_action = QAction("Exit", self)
+            exit_action.setShortcut(QKeySequence.Quit)
+            exit_action.triggered.connect(self.close)
+            
+            file_menu.addAction(exit_action)
 
 
 app = QApplication(sys.argv)
-
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
