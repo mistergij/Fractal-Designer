9fc8ca24dc712f3a34f19ee1514b0c3165b6d8d4
mistergij <57270253+mistergij@users.noreply.github.com>
2025-11-05 18:36:16 -0500

    Define Sierpinski Triangle as default shape

--- fractal_designer/shapes/sierpinski_triangle.py
+++ fractal_designer/shapes/sierpinski_triangle.py
@@ -1,19 +1,13 @@
-import numpy as np
-
-from custom_types import assert_transformation_shape
 from fractal_designer.IFS import IFS
-from shapes.square import Square
+from fractal_designer.shapes.square import Square
 
-from fractal_designer.mixins.transformer import Transformer
+from fractal_designer.transformers.polygon_transformer import PolygonTransformer
 
 
-class SierpinskiTriangle(Transformer):
+class SierpinskiTriangle(PolygonTransformer):
     def __init__(self):
         super().__init__()
         self.polygons: list[Square] = [Square("r"), Square("g"), Square("b")]
         self.add_transformation(IFS(0.5, 0, 0, 0.5, 0, 0))
         self.add_transformation(IFS(0.5, 0, 0, 0.5, 0.25, 0.5))
         self.add_transformation(IFS(0.5, 0, 0, 0.5, 0.5, 0))
-    
-    def apply_transformation(self, shape_to_transform, iterations: int):
-        polygon = assert_transformation_shape(shape_to_transform, (3, 4))
--- fractal_designer/shapes/square.py
+++ fractal_designer/shapes/square.py
@@ -1,11 +1,9 @@
-from dataclasses import dataclass
-
 import numpy as np
 
 from fractal_designer.custom_types import Float64NDArray
 
 
-@dataclass
 class Square:
-    color: str
-    points: Float64NDArray = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]).T
+    def __init__(self, color: str):
+        self.color = color
+        self.points: Float64NDArray = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]).T
