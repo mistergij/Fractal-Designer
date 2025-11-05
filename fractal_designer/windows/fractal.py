ad2b878e92cb630a9b66500b1d839a83124ad09d
mistergij <57270253+mistergij@users.noreply.github.com>
2025-11-05 18:37:38 -0500

    Plot Sierpinski Triangle by default

--- fractal_designer/windows/fractal.py
+++ fractal_designer/windows/fractal.py
@@ -4,7 +4,10 @@ import numpy as np
 from matplotlib.backends.backend_qt import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+from matplotlib.patches import Polygon
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+
+from fractal_designer.shapes.sierpinski_triangle import SierpinskiTriangle
 
 
 class FractalWindow(QMainWindow):
@@ -20,9 +23,15 @@ class FractalWindow(QMainWindow):
         layout.addWidget(static_canvas)
         layout.addWidget(NavigationToolbar(static_canvas, self))
 
+        triangle = SierpinskiTriangle()
+        triangle_points = triangle.apply_transformations(5)
+
         self._static_ax = static_canvas.figure.subplots()
-        t = np.linspace(0, 10, 501)
-        self._static_ax.plot(t, np.tan(t), ".")
+
+        for w in triangle_points:
+            for i, shape in enumerate(w):
+                patch = Polygon(shape[0:2, :].T, facecolor=triangle.polygons[i % 3].color, edgecolor="k")
+                self._static_ax.add_patch(patch)
 
         self.setWindowTitle("Fractal")
 
