eebb6c16b12a1eff4dac8c3e078b0a5e35883da3
MisterGij <57270253+mistergij@users.noreply.github.com>
2025-11-05 13:12:57 -0500

    Ensure single instance of window

--- fractal_designer/__main__.py
+++ fractal_designer/__main__.py
@@ -5,8 +5,12 @@ from PySide6.QtWidgets import QApplication
 from fractal_designer.graphics import MainWindow
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
+    
     window = MainWindow()
     window.show()
-
+    window.activateWindow()
+    window.raise_()
     app.exec()
