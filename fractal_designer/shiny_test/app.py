ece7d45f0de5263b78a917c374c32721ba1890fb
MisterGij <57270253+mistergij@users.noreply.github.com>
2025-11-13 15:58:54 -0500

    Partially implement adding transformations

--- fractal_designer/shiny_test/app.py
+++ fractal_designer/shiny_test/app.py
@@ -3,55 +3,23 @@ from matplotlib.patches import Polygon
 import numpy as np
 from shiny import reactive
 from shiny.ui import card
-from shiny.express import input, render, ui
+from shiny.express import input, render, ui, output
 
-transformation_ids: dict[int: bool] = {0: False}
+num_transformations = reactive.value([])
+transformations = reactive.value([])
 
 with ui.sidebar():
-    with ui.card(id="transformation_0"):
-        ui.card_header("Transformation 1")
-        transformation_ids[0] = False
-        ui.input_numeric("a_0", "a", 0.5, min=-1, max=1, step=0.1)
-        ui.input_numeric("b_0", "b", 0, min=-1, max=1, step=0.1)
-        ui.input_numeric("c_0", "c", 0, min=-1, max=1, step=0.1)
-        ui.input_numeric("d_0", "d", 0.5, min=-1, max=1, step=0.1)
-        ui.input_numeric("e_0", "e", 0, min=-1, max=1, step=0.1)
-        ui.input_numeric("f_0", "f", 0, min=-1, max=1, step=0.1)
-        ui.input_numeric("p_0", "p", 0, min=-1, max=1, step=0.1)
-        ui.input_action_button("add_0", "Add Transformation")
-
+    ui.input_action_button("add", "Add Transformation")
+    ui.input_action_button("remove", "Remove Last Transformation")
+    ui.input_action_button("replot", "Plot Transformations")
     ui.input_numeric("iterations", "Number of Iterations", 1, min=1, max=8)
 
-    # for id, added in transformation_ids.items():
-    #     if not added:
-    #         @reactive.effect
-    #         @reactive.event(input[f"add_{id}"])
-    #         def _():
-    #             ui.insert_ui(
-    #                 card(
-    #                     ui.card_header(f"Transformation {id + 1}"),
-    #                     ui.input_numeric(f"a_{id + 1}", "a", 1, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"b_{id + 1}", "b", 0, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"c_{id + 1}", "c", 0, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"d_{id + 1}", "d", 1, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"e_{id + 1}", "e", 0, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"f_{id + 1}", "f", 0, min=-1, max=1, step=0.1),
-    #                     ui.input_numeric(f"p_{id + 1}", "p", 0, min=-1, max=1, step=0.1),
-    #                     ui.input_action_button(f"add_{id + 1}", "Add Transformation"),
-    #                     id=f"transformation_{id + 1}",
-    #                 ),
-    #                 selector=f"#transformation_{id}",
-    #                 where="afterEnd",
-    #             )
-    #             transformation_ids[id] = True
-    #             transformation_ids[id + 1] = False
 
-
+@output
 @render.plot(alt="A Fractal")
 def plot():
-    transformations = []
-    for i in transformation_ids:
-        transformations.append(
+    for i in range(len(num_transformations.get())):
+        transformations.get().append(
             np.array(
                 [
                     [input[f"a_{i}"](), input[f"b_{i}"](), input[f"e_{i}"]()],
@@ -61,24 +29,61 @@ def plot():
             )
         )
 
-    points = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]).T
+    points = reactive.value(np.array([[0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]).T)
 
-    old_points = [points]
+    old_points = [points.get()]
     new_points = []
     colors = [(1, 0, 0, 0.5), (0, 1, 0, 0.5), (0, 0, 1, 0.5)]
 
     for i in range(1, input.iterations() + 1):
         new_points = []
         for polygon in old_points:
-            for transformation in transformations:
+            for transformation in transformations.get():
                 new_points.append(transformation @ polygon)
 
         old_points = new_points
 
     fig, ax = plt.subplots()
 
+
     for i, w in enumerate(new_points):
         patch = Polygon(w[0:2, :].T, facecolor=colors[i % 3])
         ax.add_patch(patch)
 
     return fig
+
+
+@reactive.effect
+@reactive.event(input.add)
+def add_transformation():
+    i = reactive.value(len(num_transformations.get()))
+    ui.insert_ui(
+        card(
+            ui.card_header(f"Transformation {i.get() + 1}"),
+            ui.input_numeric(f"a_{i.get()}", "a", 1, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"b_{i.get()}", "b", 0, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"c_{i.get()}", "c", 0, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"d_{i.get()}", "d", 1, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"e_{i.get()}", "e", 0, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"f_{i.get()}", "f", 0, min=-1, max=1, step=0.1),
+            ui.input_numeric(f"p_{i.get()}", "p", 0, min=-1, max=1, step=0.1),
+            id=f"transformation_{i.get()}",
+        ),
+        selector="#add",
+        where="beforeBegin",
+    )
+    num_transformations.get().append(0)
+
+
+@reactive.effect
+@reactive.event(input.remove)
+def remove_transformation():
+    ui.remove_ui(selector=".card:last")
+    if num_transformations.get():
+        num_transformations.get().pop()
+
+
+@reactive.effect
+@reactive.event(input.replot)
+def replot_transformations():
+    transformations.set([])
