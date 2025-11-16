75100922e970502bf0fedc131526ce9198a8263d
mistergij <57270253+mistergij@users.noreply.github.com>
2025-11-16 11:33:48 -0500

    Enable arbitrary amount of transformations

--- fractal_designer/shiny_test/app_core.py
+++ fractal_designer/shiny_test/app_core.py
@@ -5,22 +5,39 @@ from matplotlib.patches import Polygon
 from shiny import App, Inputs, Outputs, Session, module, reactive, render, ui
 
 
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        # [create_transformation(f"transformation_{i}") for i in range(3)],
+        ui.output_ui("create_transformation"),
+        ui.input_numeric("iterations", "Number of Iterations", 1, min=1, max=8),
+        ui.input_action_button("add_transformation", "Add Transformation"),
+    ),
+    ui.output_plot("plot"),
+)
+
+num_transformations: reactive.Value[int] = reactive.value(0)
+transformation_servers: reactive.Value[list[reactive.Value[list[reactive.Value[float]]]]] = reactive.value([])
+
+
 @module.ui
-def create_transformation(transformation_num: int = 0) -> ui.Tag:
+def transformation_card(transformation_num: int = 0) -> ui.Tag:
     return ui.card(
-                ui.card_header(f"Transformation {transformation_num}"),
-                ui.input_numeric("a", "a", 1, min=-1, max=1, step=0.1),
-                ui.input_numeric("b", "b", 0, min=-1, max=1, step=0.1),
-                ui.input_numeric("c", "c", 0, min=-1, max=1, step=0.1),
-                ui.input_numeric("d", "d", 1, min=-1, max=1, step=0.1),
-                ui.input_numeric("e", "e", 0, min=-1, max=1, step=0.1),
-                ui.input_numeric("f", "f", 0, min=-1, max=1, step=0.1),
-                ui.input_numeric("p", "p", 0, min=-1, max=1, step=0.1),
-                id="transformation",
-            )
+        ui.card_header(f"Transformation {transformation_num}"),
+        ui.input_numeric("a", "a", 1, min=-1, max=1, step=0.1),
+        ui.input_numeric("b", "b", 0, min=-1, max=1, step=0.1),
+        ui.input_numeric("c", "c", 0, min=-1, max=1, step=0.1),
+        ui.input_numeric("d", "d", 1, min=-1, max=1, step=0.1),
+        ui.input_numeric("e", "e", 0, min=-1, max=1, step=0.1),
+        ui.input_numeric("f", "f", 0, min=-1, max=1, step=0.1),
+        ui.input_numeric("p", "p", 0, min=-1, max=1, step=0.1),
+        id="transformation",
+    )
+
 
 @module.server
-def transformation_server(input: Inputs, output: Outputs, session: Session) -> reactive.Value[list[reactive.Value[float]]]:
+def transformation_server(
+    input: Inputs, output: Outputs, session: Session
+) -> reactive.Value[list[reactive.Value[float]]]:
     transformation = reactive.value(
         [
             input.a,
@@ -37,14 +54,13 @@ def transformation_server(input: Inputs, output: Outputs, session: Session) -> r
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    server_transformations: list[reactive.Value[list[reactive.Value[float]]]] = []
-    for i in range(3):
-        server_transformations.append(transformation_server(f"transformation_{i}"))
-    @render.plot(alt="A fractal")
-    def plot():
+    @reactive.calc
+    def compute_transformation():
+        input.add_transformation()
+        _transformation_servers = transformation_servers.get()
         transformations: list[np.typing.NDArray[np.float32]] = []
-        
-        for server in server_transformations:
+
+        for server in _transformation_servers:
             transformations.append(
                 np.array(
                     [
@@ -59,17 +75,23 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         old_points = [points]
         new_points: list[np.typing.NDArray[np.float32]] = []
-        colors = [(1, 0, 0, 0.5), (0, 1, 0, 0.5), (0, 0, 1, 0.5)]
 
-        for i in range(1, input.iterations() + 1):
+        for _ in range(1, input.iterations() + 1):
             new_points = []
             for polygon in old_points:
                 for transformation in transformations:
                     new_points.append(transformation @ polygon)
 
-            old_points = new_points
+                old_points = new_points
 
-        fig, ax = plt.subplots() # pyright: ignore [reportUnknownMemberType]
+        return new_points
+
+    @render.plot(alt="A fractal")
+    def plot():
+        new_points: list[np.typing.NDArray[np.float32]] = compute_transformation()
+        colors = [(1, 0, 0, 0.5), (0, 1, 0, 0.5), (0, 0, 1, 0.5)]
+
+        fig, ax = plt.subplots()  # pyright: ignore [reportUnknownMemberType]
 
         for i, w in enumerate(new_points):
             patch = Polygon(w[0:2, :].T, facecolor=colors[i % 3])
@@ -77,12 +99,22 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         return fig
 
-app_ui = ui.page_sidebar(
-    ui.sidebar(
-        [create_transformation(f"transformation_{i}") for i in range(3)],
-        ui.input_numeric("iterations", "Number of Iterations", 1, min=1, max=8),
-    ),
-    ui.output_plot("plot"),
-)
+    @render.ui
+    @reactive.event(input.add_transformation)
+    def create_transformation():
+        _num_transformations = num_transformations.get()
+        transformation_cards = [transformation_card(f"transformation_{i}", i) for i in range(_num_transformations + 1)]
+        num_transformations.set(_num_transformations + 1)
+
+        return transformation_cards
+
+    @reactive.effect
+    def create_transformation_servers():
+        _num_transformations = num_transformations.get()
+        _transformation_servers = transformation_servers.get()
+        if _num_transformations != 0 and _num_transformations < 8:
+            _transformation_servers.append(transformation_server(f"transformation_{_num_transformations - 1}"))
+            transformation_servers.set(_transformation_servers)
+
 
 app = App(app_ui, server)
